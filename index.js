00c001274dac83b4f7bde56681e560b32c395535
neyugn21 <thanhntph47600@gmail.com>
2024-09-10 17:02:22 +0700

    firstcm

---
+++ index.js
@@ -0,0 +1,15 @@
+// khoi tao thong tin co ban du an: npm init
+// cai dat thu can thiet: npm i
+let name = "harry";
+console.log(name);
+
+let obj = {
+  name: "Thanh nguyen",
+  age: 24,
+  address: "Ha Noi",
+};
+console.log(obj);
+
+let arr = [1, 2, 3];
+let [a, b, c] = arr;
+console.log(a, b, c);
